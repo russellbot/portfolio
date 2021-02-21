688682e850dfe6991a33112e27a80d9d309009ff
Russell Hart <russellbot@gmail.com>
2021-02-21 16:45:40 -0500

    added toggle to mobile navbar

--- react-portfolio/src/components/Navbar/index.js
+++ react-portfolio/src/components/Navbar/index.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import {FaBars} from 'react-icons/fa'
+import React from "react";
+import { FaBars } from "react-icons/fa";
 import {
-    Nav, 
-    NavbarContainer, 
-    NavLogo,
-    MobileIcon, 
-    NavMenu, 
-    NavItem, 
-    NavLinks,
-    NavBtn,
-    NavBtnLink
-} from './NavbarElements';
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
-const Navbar = () => {
-    return (
-        <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to='/'>rh</NavLogo>
-                    <MobileIcon>
-                        <FaBars />
-                    </MobileIcon>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLinks to="about">About</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="discover">Discover</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="services">Services</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to="signup">Sign Up</NavLinks>
-                        </NavItem>
-                        <NavBtn>
-                            <NavBtnLink t="/signin">Sign In</NavBtnLink>
-                        </NavBtn>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
-        </>
-    )
-}
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">rh</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="work">Work</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="contact">Contact</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+            <NavBtn>
+              <NavBtnLink t="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavMenu>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
 
 export default Navbar;
